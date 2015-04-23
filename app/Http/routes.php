1a702c70a0962c988ab122a88526062926cb7710
Slaven Sakacic <slavensakacic@gmail.com>
2015-04-23 12:51:16 +0200

    Fix

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,15 +17,28 @@ Route::get('home', 'HomeController@index');
 
 Route::get('homepage', function()
 {
-   $users_published_tests =DB::table('users')
-    ->join('tests', 'users.id', '=', 'tests.user_id')
-    ->where('is_published','=', 1)
-    ->select('users.name', 'tests.id', 'tests.test_name',
-        'tests.intro', 'tests.conclusion', 'tests.shuffle',
-        'tests.user_id', 'tests.is_published','tests.is_public',
-        'tests.intro_image', 'tests.conclusion_image')
-    ->get();
-    return view('welcome')->with('users_published_tests', $users_published_tests);
+   $users_published_tests_public =DB::table('users')
+                ->join('tests', 'users.id', '=', 'tests.user_id')
+                ->where('tests.is_published','=', 1)
+                ->where('tests.is_public','=', 1)
+                ->select('users.name', 'tests.id', 'tests.test_name',
+                       'tests.intro', 'tests.conclusion', 'tests.shuffle',
+                       'tests.user_id', 'tests.is_published','tests.is_public',
+                       'tests.intro_image', 'tests.conclusion_image')
+                ->get();
+
+            $users_published_tests_private =DB::table('users')
+                ->join('tests', 'users.id', '=', 'tests.user_id')
+                ->where('tests.is_published','=', 1)
+                ->where('tests.is_public','=', 0)
+                ->select('users.name', 'tests.id', 'tests.test_name',
+                       'tests.intro', 'tests.conclusion', 'tests.shuffle',
+                       'tests.user_id', 'tests.is_published','tests.is_public',
+                       'tests.intro_image', 'tests.conclusion_image')
+                ->get();
+        return view('welcome')
+        ->with('users_published_tests_public', $users_published_tests_public)
+        ->with('users_published_tests_private',$users_published_tests_private);
 });
 
 Route::get('tests', array('as' => 'tests', 'middleware' => 'auth', 'uses' => 'TestController'));
